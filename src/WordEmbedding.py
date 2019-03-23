932ec6b2c1d0339727ef9f4e4fa4c80cb246e33d
bernaerden <berer348>
2019-03-23 14:37:36 +0300

    added fasttext embedding loader

--- src/WordEmbedding.py
+++ src/WordEmbedding.py
@@ -1,6 +1,7 @@
 import gzip
 import numpy as np
 import logging
+import fastText as fasttext
 
 
 def load_all_word_embeding_dictionary(path):
@@ -45,7 +46,7 @@ def find_word_index(we_words, word):
         return False
 
 
-def read_fastText_word_embeddings(we_path, words, n_words, embedding_size=300):
+def read_fastText_word_embeddings_old(we_path, words, n_words, embedding_size=300):
     we_dict = get_word_embeding_dictionary(we_path)
     word_vector_indexes = dict()
     unk_word_vector = [round(np.random.uniform(-0.1, 0.1), 5) for i in range(300)]
@@ -82,5 +83,35 @@ def read_fastText_word_embeddings(we_path, words, n_words, embedding_size=300):
             embedding_matrix[v_idx] = np.array(embedding_vector)
     n_unk_words = len(unk_word_vector_indexes)
     logging.info('Number of found tokens: %s' % n_words - n_unk_words)
+    logging.info('Number of unknown tokens: %s' % n_unk_words)
+    return embedding_matrix, unk_word_vector
+
+
+def read_fastText_word_embeddings(we_path, words, n_words, embedding_size=300):
+    model = fasttext.load_model(we_path)
+    model_words = model.get_words()
+    unk_word_vector = [round(np.random.uniform(-0.1, 0.1), 5) for i in range(300)]
+    sub_word_indexes = []
+    unk_word_indexes = []
+    embedding_matrix = np.zeros((n_words, embedding_size))
+    for vocab_idx, word in enumerate(words):
+        if word == '<UNK>':
+            embedding_matrix[vocab_idx] = np.array(unk_word_vector)
+            continue
+        if word in model_words:
+            embedding_vector = model.get_word_vector(word)
+            embedding_matrix[vocab_idx] = np.array(embedding_vector)
+        else:
+            try:
+                embedding_vector = model.get_word_vector(word)
+                embedding_matrix[vocab_idx] = np.array(embedding_vector)
+                sub_word_indexes.append(vocab_idx)
+            except:
+                embedding_matrix[vocab_idx] = np.array(unk_word_vector)
+                unk_word_indexes.append(vocab_idx)
+    n_sub_words = len(sub_word_indexes)
+    n_unk_words = len(unk_word_indexes)
+    logging.info('Number of tokens: %s' % n_words)
+    logging.info('Number of sub tokens: %s' % n_sub_words)
     logging.info('Number of unknown tokens: %s' % n_unk_words)
     return embedding_matrix, unk_word_vector
