9f88e50e2b8decb8ac020782f8ca45a2ed4970ec
bernaerden <berer348>
2019-03-24 22:44:16 +0300

    updated model params

--- src/Parameters.py
+++ src/Parameters.py
@@ -1,23 +1,50 @@
-model_params = {'BG': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 32, 'epochs': 12},
-                'DE': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 15},
-                'EL': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 15},
-                'EN': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 256, 'epochs': 2},#'batch_size': 8, 'epochs': 15
-                'ES': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 15},
-                'EU': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 15},
-                'FA': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 12},
-                'FR': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 32, 'epochs': 12},
-                'HE': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 32, 'epochs': 12},
-                'HI': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 15},
-                'HR': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 8, 'epochs': 15},
-                'HU': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 15},
-                'IT': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 12},
-                'LT': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 32, 'epochs': 12},
-                'PL': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 12},
-                'PT': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 32, 'epochs': 12},
-                'RO': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 32, 'epochs': 12},
-                'SL': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 12},
-                'TR': {'n_units': 20, 'dropout': [0.1, 0.1], 'batch_size': 32, 'epochs': 12}
-                }
-char_cnn_model_params = {'char_emb_size': 30, 'char_window_size': 3, 'char_filter_size': 30,
-                         'char_cnn_mask_zero': False}
-char_lstm_model_params = {'char_lstm_n_units': 25, 'char_lstm_mask_zero': True}
+model_params = {
+    'BG': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 32, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'DE': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'EL': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'EN': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 8, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'ES': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'EU': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'FA': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'FR': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 32, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'HE': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 32, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'HI': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'HR': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 8, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'HU': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'IT': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'LT': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 32, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'PL': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'PT': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 32, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'RO': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 64, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'SL': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 16, 'epochs': 25, 'patience': 5,
+           'verbose': 2},
+    'TR': {'n_units': 20, 'dropout': 0.5, 'var_dropout': [0.1, 0.1], 'batch_size': 64, 'epochs': 25, 'patience': 5,
+           'verbose': 2}
+}
+char_trigram_cnn_model_params = {'char_emb_size': 30, 'char_window_size': 3, 'char_filter_size': 30}
+char_lstm_model_params = {'char_emb_size': 30, 'char_lstm_n_units': 25}
+
+model_cfg = {
+    '01': {'SPELLING': False, 'CHAR': False, 'POS': False, 'DEPREL': False, 'DROPOUT': False, 'MORPHEME': False},
+    '02': {'SPELLING': True, 'CHAR': False, 'POS': False, 'DEPREL': False, 'DROPOUT': False, 'MORPHEME': False},
+    '03': {'SPELLING': False, 'CHAR': 'cnn', 'POS': False, 'DEPREL': False, 'DROPOUT': False, 'MORPHEME': False},
+    '04': {'SPELLING': False, 'CHAR': 'cnn', 'POS': False, 'DEPREL': False, 'DROPOUT': True, 'MORPHEME': False},
+    '05': {'SPELLING': False, 'CHAR': 'lstm', 'POS': False, 'DEPREL': False, 'DROPOUT': False, 'MORPHEME': False},
+    '06': {'SPELLING': False, 'CHAR': 'lstm', 'POS': False, 'DEPREL': False, 'DROPOUT': True, 'MORPHEME': False}, }
